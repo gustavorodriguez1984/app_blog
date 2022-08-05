91b29162533d54971877f6126841d11386670552
Gustavo Adolfo Rodríguez Beteta <grodriguez@cgclatam.com>
2022-08-05 11:29:50 -0600

    Development routes, styled components, structure of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Acerca from './components/Acerca';
+import Inicio from './components/Inicio';
+import Blog from './components/Blog';
+import Header from './components/Header';
+import styled from 'styled-components';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <ContenedorPrincipal>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/blog' element={<Blog />} />
+            <Route path='/acerca' element={<Acerca />}/>
+          </Routes>
+        </Main>
 
+      </ContenedorPrincipal>
+    </BrowserRouter>
+  );
+};
+
+const ContenedorPrincipal = styled.div`
+  padding:40px;
+  width:98%;
+  max-width:700px;
+`;
+const Main = styled.main`
+	background: #fff;
+	padding: 40px;
+	border-radius: 10px;
+	box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
 export default App;
---
+++ src/components/Acerca.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Acerca = () => {
+    return (
+        <div>
+           
+         
+              <p>hola me llamo tavo.</p>
+
+         
+        </div>
+    );
+};
+
+export default Acerca;
